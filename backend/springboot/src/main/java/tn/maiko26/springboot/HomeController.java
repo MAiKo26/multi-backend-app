bb8885d3c257bd4c53aaea15b6370ab256d55640
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2024-12-15 13:04:12 +0100

    initial commit

---
+++ backend/springboot/src/main/java/tn/maiko26/springboot/HomeController.java
@@ -0,0 +1,20 @@
+package tn.maiko26.springboot;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public Map<String, Object> greeting() {
+        return Collections.singletonMap("message", "Hello, World !!");
+    }
+    @GetMapping("/hello")
+    public Map<String,Object> hello(){
+        return Collections.singletonMap("message","Hello, World AGAIN yoo xqo xq  x dq !!");
+    }
+}
