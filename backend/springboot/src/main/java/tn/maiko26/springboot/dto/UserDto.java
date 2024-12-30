6b7ea2ba9941e9a000f98b1aaca75f8db9f4e97a
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2024-12-30 17:09:10 +0100

    Update UserDto.java

--- backend/springboot/src/main/java/tn/maiko26/springboot/dto/UserDto.java
+++ backend/springboot/src/main/java/tn/maiko26/springboot/dto/UserDto.java
@@ -2,12 +2,14 @@ package tn.maiko26.springboot.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import tn.maiko26.springboot.enums.UserRole;
+import lombok.NoArgsConstructor;
+import tn.maiko26.springboot.model.User;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
 
 
@@ -18,4 +20,10 @@ public class UserDto {
     private String role;
     private Date lastLogin;
 
+
+    public UserDto(User user) {
+        this.email = user.getEmail();
+        this.name = user.getName();
+        this.avatarUrl = user.getAvatarUrl();
+    }
 }
