d60d5e4bf36c208ee2e47813854bab9aa9f7a341
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2025-01-01 20:58:07 +0100

    feat: logging service

--- backend/dotnet/Services/ActivityHistoryService.cs
+++ backend/dotnet/Services/ActivityHistoryService.cs
@@ -1,32 +1,64 @@
 using dotnet.Data;
 using dotnet.Interfaces;
 using dotnet.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnet.Services;
 
 public class ActivityHistoryService : IActivityHistoryService
 {
-    
     private readonly DataContext _context;
+    private readonly UserService _userService; 
 
-
-    public ActivityHistoryService(DataContext context)
+    public ActivityHistoryService(DataContext context, UserService userService)
     {
         _context = context;
+        _userService = userService;
     }
-    
+   
     public IEnumerable<ActivityHistory> GetAllActivities()
     {
-        throw new NotImplementedException();
+        return _context.ActivityHistories
+            .Include(a => a.User)
+            .OrderByDescending(a => a.DoneAt)
+            .ToList();
     }
 
     public IEnumerable<ActivityHistory> GetActivityByEmail(string email)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrWhiteSpace(email))
+        {
+            throw new ArgumentException("Email cannot be null or empty");
+        }
+
+        return _context.ActivityHistories
+            .Include(a => a.User)
+            .Where(a => a.User.Email == email)
+            .OrderByDescending(a => a.DoneAt)
+            .ToList();
     }
 
-    public void LogActivity(string userId, string description)
+    public void LogActivity(string userEmail, string description)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var user = _userService.GetUserByEmail(userEmail);
+           
+            var activityHistory = new ActivityHistory
+            {
+                Id = Guid.NewGuid().ToString(),
+                User = user,
+                Description = description,
+                DoneAt = DateTime.Now
+            };
+
+            _context.ActivityHistories.Add(activityHistory);
+            _context.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine($"Failed to log activity: {e.Message}");
+            throw;
+        }
     }
 }
